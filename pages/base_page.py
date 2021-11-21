03ee1807b491e17d1bc6053387658ef90cac5129
Max <max21011999@mail.ru>
2021-11-22 00:40:16 +0300

    abskhcskj

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,6 @@
 import urllib.parse
 
+
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -9,6 +10,7 @@ from selenium.webdriver.common.by import By
 class Page(object):
     BASE_URL = 'https://qdaqda.ru/'
     PATH = ''
+    WAIT_TIME = 1.5
 
     def __init__(self, driver):
         self.driver = driver
@@ -16,6 +18,11 @@ class Page(object):
     def open(self):
         self.open(self.BASE_URL)
 
+    def wait_until(self, cond_f, wait: float = None):
+        if wait is None:
+            wait = self.WAIT_TIME
+        waiter = WebDriverWait(self.driver, wait)
+        return waiter.until(cond_f)
 
     def wait_until_and_get_elem_by_xpath(self, elem) -> WebElement:
         return WebDriverWait(self.driver, 15, 0.1).until(EC.visibility_of_element_located((By.XPATH, elem)))
@@ -30,7 +37,11 @@ class Page(object):
         return WebDriverWait(self.driver, 30, 0.1).until(EC.url_to_be(url))
 
     def wait_for_page(self, container_css):
-        return WebDriverWait(self.driver, 30, 0.1).until(EC.visibility_of_element_located((By.CSS_SELECTOR, container_css)))
+        return WebDriverWait(self.driver, 30, 0.1).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, container_css)))
+
+    def locate_hidden_el(self, css_sel, wait: float = 3.0) -> WebElement:
+        return self.wait_until(EC.presence_of_element_located((By.CSS_SELECTOR, css_sel)), wait)
 
     def open(self, url=None):
         if (url == None):
@@ -41,8 +52,3 @@ class Page(object):
 
     def refresh(self):
         self.driver.refresh()
-
-
-
-
-
--- run_tests.py
+++ run_tests.py
@@ -7,6 +7,7 @@ from cases.event_cases import EventTest
 from cases.events_cases import EventsTest
 from cases.login_cases import LoginTest
 from cases.registration_cases import RegistrationTest
+from cases.profile_cases import ProfileTest
 
 if __name__ == '__main__':
     suite = unittest.TestSuite(
@@ -14,7 +15,8 @@ if __name__ == '__main__':
         # unittest.makeSuite(LoginTest),
         # unittest.makeSuite(RegistrationTest),
         # unittest.makeSuite(EventsTest),
-        unittest.makeSuite(EventTest)
+        # unittest.makeSuite(EventTest),
+        unittest.makeSuite(ProfileTest)
     )
     )
 
